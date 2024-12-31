f5f871d7b6c42078d4a9f39fd9e165d8847b5b86
Ray Myers <ray.myers@gmail.com>
2024-12-30 18:13:22 -0600

    Config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Ray Myers";
+export const SITE_DESCRIPTION = "Retrofuturist software mender";
